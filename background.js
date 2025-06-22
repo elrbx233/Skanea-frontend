98a7498c6522ecac96fb0b6338b25c5db51b5ed0
elrbx233 <juanvizh32@gmail.com>
2025-06-22 16:24:07 -0500

    Replace screen-based popup positioning

--- background.js
+++ background.js
@@ -1,10 +1,15 @@
 chrome.action.onClicked.addListener(() => {
-  chrome.windows.create({
-    url: 'popup.html',
-    type: 'popup',
-    width: 360,
-    height: 500,
-    top: 70,
-    left: screen.availWidth - 400
+  chrome.system.display.getInfo((displays) => {
+    const display = displays[0];
+    const leftPos = display.workArea.width - 400;
+
+    chrome.windows.create({
+      url: 'popup.html',
+      type: 'popup',
+      width: 360,
+      height: 500,
+      top: 70,
+      left: leftPos
+    });
   });
 });
